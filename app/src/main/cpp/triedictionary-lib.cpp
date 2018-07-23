001fd529a19f5e1d253f84f5b938028f078e9d13
Hyegyu Lee <regyu1k02@gmail.com>
2018-07-23 18:09:32 +0900

    Add searchStartsWith.

--- app/src/main/cpp/triedictionary-lib.cpp
+++ app/src/main/cpp/triedictionary-lib.cpp
@@ -58,32 +58,50 @@ Java_me_blog_hgl1002_openwnn_KOKR_trie_NativeTrie_searchNative(JNIEnv * jenv, jo
     return (jboolean) (p != nullptr && (fitLength && p->frequency == 0));
 }
 
+std::list<std::wstring> * searchStartsWith(TrieNode * p, std::wstring prefix, std::list<std::wstring> * list, std::wstring currentWord, int depth, int limit) {
+    if(prefix.length() <= 1) return list;
+    if(limit > 0 && currentWord.length() > limit) return list;
+    if(p->compressed != nullptr) {
+        if(limit == 0 || depth < limit) limit = depth;
+        if(limit > 0 && currentWord.length() + p->compressed->length() - 1 > limit) return list;
+        list->push_back(currentWord + p->compressed->substr(1));
+        return list;
+    }
+    if(p->frequency > 0 && depth >= prefix.length()) {
+        list->push_back(currentWord);
+    }
+    wchar_t ch = prefix[depth];
+    if(p->children != nullptr) {
+        if(p->children->count(ch)) {
+            searchStartsWith(p->children->find(ch)->second, prefix, list, currentWord + ch, depth+1, limit);
+        } else if(depth >= prefix.length()) {
+            for(auto it = p->children->begin() ; it != p->children->end() ; it++) {
+                searchStartsWith(it->second, prefix, list, currentWord + it->second->ch, depth+1, limit);
+            }
+        }
+    }
+    return list;
+}
+
 JNIEXPORT jobjectArray JNICALL
 Java_me_blog_hgl1002_openwnn_KOKR_trie_NativeTrie_searchStartsWithNative(JNIEnv * jenv, jobject self, jstring word_, jint limit) {
     const char * chars = jenv->GetStringUTFChars(word_, JNI_FALSE);
     size_t length = strlen(chars) + 1;
     wchar_t * word = (wchar_t*) malloc(sizeof(wchar_t) * length);
     mbstowcs(word, chars, length);
-    TrieNode * p = searchNode(getRoot(jenv, self), word, length);
-    if(p != nullptr) {
-        std::list<std::wstring> * list = getAllWords(p, new std::list<std::wstring>(), std::wstring(word), limit);
-        jobjectArray result = jenv->NewObjectArray((jsize) list->size(), jenv->FindClass("java/lang/String"), jenv->NewStringUTF(""));
-        for(auto it = list->begin() ; it != list->end() ; it++) {
-            int index = (int) std::distance(list->begin(), it);
-            if(index > 256) break;
-            size_t len = it->length() * sizeof(wchar_t) + 1;
-            char str[len];
-            wcstombs(str, it->c_str(), len);
-            jenv->SetObjectArrayElement(result, (jsize) index, jenv->NewStringUTF(str));
-        }
-        delete list;
-        free(word);
-        return result;
-    } else {
-        jobjectArray result = jenv->NewObjectArray((jsize) 0, jenv->FindClass("java/lang/String"), jenv->NewStringUTF(""));
-        free(word);
-        return result;
+    std::list<std::wstring> * list = searchStartsWith(getRoot(jenv, self), word, new std::list<std::wstring>(), std::wstring(), 0, 0);
+    jobjectArray result = jenv->NewObjectArray((jsize) list->size(), jenv->FindClass("java/lang/String"), jenv->NewStringUTF(""));
+    for(auto it = list->begin() ; it != list->end() ; it++) {
+        int index = (int) std::distance(list->begin(), it);
+        if(index > 256) break;
+        size_t len = it->length() * sizeof(wchar_t) + 1;
+        char str[len];
+        wcstombs(str, it->c_str(), len);
+        jenv->SetObjectArrayElement(result, (jsize) index, jenv->NewStringUTF(str));
     }
+    delete list;
+    free(word);
+    return result;
 }
 
 JNIEXPORT jobjectArray JNICALL
